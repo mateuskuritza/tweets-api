fd8961274102579b67fa656a286fd9a09390271f
mateuskuritza <mateuskuritza@gmail.com>
2024-09-29 00:51:42 -0300

    feat: adds UUID validation

--- src/main/java/com/tweet/api/dtos/TweetDTO.java
+++ src/main/java/com/tweet/api/dtos/TweetDTO.java
@@ -1,17 +1,18 @@
 package com.tweet.api.dtos;
 
-import java.util.UUID;
-
 import com.tweet.api.models.Tweet;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record TweetDTO(
-        @NotNull UUID userId,
+        @NotBlank @NotNull
+        // uuid
+        @Pattern(message = "userId must be valid UUID", regexp = "^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$") String userId,
         @NotBlank @NotNull String content) {
 
     public TweetDTO(Tweet tweet) {
-        this(tweet.getUser().getId(), tweet.getText());
+        this(tweet.getUser().getId().toString(), tweet.getText());
     }
 }
